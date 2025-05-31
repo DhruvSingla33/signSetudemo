915ee0f3d6814647a1b559ec16b228cb5e12ac84
DhruvSingla33 <singladhruv301@gmail.com>
2025-05-31 23:44:26 +0530

    css updated

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Upload.css";
+import "./upload.css";
 
 const Upload = ({ setOpen }) => {
   const [img, setImg] = useState(undefined);
